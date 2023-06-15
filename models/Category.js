9fb484fe5c418391645cbf6901549d0a1af5ecdf
Lucas Schvindt <lucas.schvindt@gmail.com>
2023-06-15 12:38:28 -0400

    Added models columns

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, // integer
+      allowNull: false, // doesn't allow null values
+      primaryKey: true, // set as primary key
+      autoIncrement: true // uses auto increment
+    },
+    category_name: {
+      type: DataTypes.STRING, // string
+      allowNull: false // doesn't allow null values
+    }
   },
   {
     sequelize,
