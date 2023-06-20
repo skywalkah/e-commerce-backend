eba9d92c702444ddd8b5dba757abff96110fddd3
Lucas Schvindt <lucas.schvindt@gmail.com>
2023-06-20 17:26:47 -0400

    Added sequelize connection and synced the models to the db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,4 +14,11 @@ app.use(routes);
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false }) // force: true will drop the table if it already exists
+    .then(() => {
+      console.log('Database & tables created!');
+    })
+    .catch(err => {
+      console.error('Unable to connect to the database:', err);
+    });
 });
